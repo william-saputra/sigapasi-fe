6ef4d6a3a32ffa6af7654cb31b6afee63889f6f9
Salomo Immanuel Putra <salomo.immanuel@ui.ac.id>
2026-03-09 20:36:32 +0700

    Add FE

---
+++ src/services/schedule/availability.service.ts
@@ -0,0 +1,16 @@
+import apiService from '../api.service'
+import type { TeacherAvailabilityResponseDTO, BulkAvailabilityRequestDTO } from '@/interfaces/schedules/availability.types'
+
+export const availabilityService = {
+    // Mengambil data ketersediaan guru yang sedang login
+    getAvailability: () => 
+        apiService.get<TeacherAvailabilityResponseDTO>('/availability'),
+
+    // Membuat data baru (jika belum pernah isi sama sekali)
+    createAvailability: (data: BulkAvailabilityRequestDTO) => 
+        apiService.post<any>('/availability', data),
+
+    // Memperbarui data ketersediaan (jika sudah pernah isi)
+    updateAvailability: (data: BulkAvailabilityRequestDTO) => 
+        apiService.put<any>('/availability', data),
+}
