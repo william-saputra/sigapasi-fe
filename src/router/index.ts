8060b4c18f8acbd1761af22f3b0b8ed43e8c7cb6
William Matthew Saputra <158471021+william-saputra@users.noreply.github.com>
2026-04-27 15:31:01 +0700

    Disable active semester check in router
    Comment out the active semester check and redirect logic.

--- src/router/index.ts
+++ src/router/index.ts
@@ -237,12 +237,12 @@ router.beforeEach(async (to, from, next) => {
       await academicSetupStore.fetchActiveSetup()
     }
 
-    if (!academicSetupStore.activeSemester) {
-      if (to.path !== '/setup-academic') {
-        // Peringatkan user atau redirect
-        return next({ path: '/setup-academic' })
-      }
-    }
+    // if (!academicSetupStore.activeSemester) {
+    //   if (to.path !== '/setup-academic') {
+    //     // Peringatkan user atau redirect
+    //     return next({ path: '/setup-academic' })
+    //   }
+    // }
   }
 
   next()
